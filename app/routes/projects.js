183f4323a42c2ba1dcc20270b18befd02415625c
Jason Blanchard <jas.blanchard@gmail.com>
2015-01-03 09:52:56 -0500

    Added projects

--- app/routes/projects.js
+++ app/routes/projects.js
@@ -6,14 +6,14 @@ export default Ember.Route.extend({
       {
         id: 1,
         name: "ApprenNet",
-        description: "Description blah blah blah ....",
+        description: "ApprenNet is a scenario-based learning app that presents learners with exercises using video and peer assessment. The Web app stack includes Ruby on Rails, MongoDB and Angular.js. ApprenNet also has a mobile client built with Cordova and Angular, and a desktop app built with Node Webkit.",
         url: "https://www.apprennet.com",
         code: ''
       },
       {
         id: 2,
         name: "Writermortis",
-        description: "Blah dee blah",
+        description: "Writermortis is an online writing game that works like an \"exquisite corpse\" - one writer begins the story and sends it to other writers. Each subsequent writer can only see the previously written section. Writermortis is currently a Ruby on Rails app but is going through an extensive re-write.",
         url: "http://www.writermortis.com/",
         code: "https://github.com/jasonblanchard/writermortis"
       }
