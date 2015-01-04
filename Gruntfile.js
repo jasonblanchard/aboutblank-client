ad2bb1f72e99af20359c73a1257358f84d0c7321
Jason Blanchard <jas.blanchard@gmail.com>
2015-01-04 11:05:28 -0500

    Added deploy grunt task

---
+++ Gruntfile.js
@@ -0,0 +1,31 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+
+    aws: grunt.file.readJSON('s3.json'),
+    aws_s3: {
+      options: {
+        accessKeyId: '<%= aws.AWSAccessKeyId %>',
+        secretAccessKey: '<%= aws.AWSSecretKey %>',
+        uploadConcurrency: 5,
+        region: 'us-west-1',
+        endpoint: 's3.amazonaws.com'
+      },
+
+      production: {
+        options: {
+          bucket: 'aboutblanktech',
+        },
+        files: [
+          {expand: true, cwd: 'dist/', src: ['**'], dest: ''}
+        ]
+      }
+    }
+
+  });
+
+  grunt.loadNpmTasks('grunt-aws-s3');
+
+  grunt.registerTask('deploy', ['aws_s3']);
+
+};
