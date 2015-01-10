af8d66762bd222d4b9800030c968fe5f2e8ea7f3
Jason Blanchard <jas.blanchard@gmail.com>
2015-01-10 16:07:30 -0500

    Configuring api host per environment

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,10 +1,15 @@
 import DS from 'ember-data';
+import ENV from 'aboutblank/config/environment';
 
 export default DS.ActiveModelAdapter.extend({
   namespace: 'api/v2'
 });
 
 DS.ActiveModelAdapter.reopen({
-  host: 'https://aboutblank-server.herokuapp.com'
+  if (ENV.environment === 'development') {
+    host: 'localhost:3000'
+  } else {
+    host: 'https://aboutblank-server.herokuapp.com'
+  }
 });
 
