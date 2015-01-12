414716d62ea83f105f84a2cc468bf28eae72619e
Jason Blanchard <jas.blanchard@gmail.com>
2015-01-12 13:16:22 -0500

    Added event toggle

--- app/controllers/feed/index.js
+++ app/controllers/feed/index.js
@@ -2,7 +2,22 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
 
-  activeEvents: ['deliciousEvents', 'goodreadsEvents'],
+  availableEvents: function() {
+    
+    // TODO: Refactor this to use the model classes somehow
+    var eventTypes = [
+      Ember.ObjectProxy.create({content: {name: 'deliciousEvents', active: true, icon: 'icon-delicious'}}),
+      Ember.ObjectProxy.create({content: {name: 'goodreadsEvents', active: true, icon: 'icon-book'}})
+    ];
+
+    return Ember.ArrayProxy.create({
+      content: eventTypes
+    });
+  }.property('model'),
+
+  activeEvents: Ember.computed.filterBy('availableEvents', 'active', true),
+
+  //activeEvents: ['deliciousEvents', 'goodreadsEvents'],
 
   allEvents: function() {
     return this.get('model').get('firstObject');
@@ -16,13 +31,13 @@ export default Ember.Controller.extend({
     var feed = Ember.A();
 
     this.get('activeEvents').forEach(function(item) {
-      events.get(item).forEach(function(eventItem) {
+      events.get(item.get('name')).forEach(function(eventItem) {
         feed.pushObject(eventItem);
       });
     });
     
     return feed.sortBy('happenedAt').reverse();
 
-  }.property('allEvents.@each', 'activeEvents')
+  }.property('allEvents.@each', 'activeEvents.@each')
 
 });
