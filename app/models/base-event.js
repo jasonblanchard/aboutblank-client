770fdf6d1d5ced3e9797b4fa94c8d3c8af47c54d
Jason Blanchard <jas.blanchard@gmail.com>
2015-01-16 17:36:53 -0500

    DRYd up baseEvent

--- app/models/base-event.js
+++ app/models/base-event.js
@@ -1,9 +1,16 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
+  metadata: Ember.Object.create({icon: '', prefix: ''}),
   title: DS.attr('string'),
   uuid: DS.attr('string'),
   happenedAt: DS.attr('date'),
   imageUrl: DS.attr('string'),
   url: DS.attr('string'),
+  icon: function() {
+    return this.get('metadata').get('icon');
+  }.property(),
+  prefix: function() {
+    return this.get('metadata').get('prefix');
+  }.property()
 });
--- app/models/goodreads-event.js
+++ app/models/goodreads-event.js
@@ -1,9 +1,6 @@
 import BaseEvent from 'aboutblank/models/base-event';
 import registeredEvents from 'aboutblank/models/registered-events';
 
-var metadata = registeredEvents.findBy('name','goodreadsEvents');
-
 export default BaseEvent.extend({
-  icon: metadata.get('icon'),
-  prefix: metadata.get('prefix')
+  metadata: registeredEvents.findBy('name','goodreadsEvents')
 });
