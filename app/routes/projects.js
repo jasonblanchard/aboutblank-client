44ceddc633451b5219488905be95a2d2e0e18f6a
Jason Blanchard <jas.blanchard@gmail.com>
2015-01-21 08:25:41 -0500

    Added screenshots

--- app/routes/projects.js
+++ app/routes/projects.js
@@ -4,18 +4,20 @@ export default Ember.Route.extend({
   model: function() {
     return [
       {
-        id: 1,
-        name: "ApprenNet",
-        description: "ApprenNet is a scenario-based learning app that presents learners with exercises using video and peer assessment. The Web app stack includes Ruby on Rails, MongoDB and Angular.js. ApprenNet also has a mobile client built with Cordova and Angular, and a desktop app built with Node Webkit.",
-        url: "https://www.apprennet.com",
-        code: ''
-      },
-      {
         id: 2,
         name: "Writermortis",
-        description: "Writermortis is an online writing game that works like an \"exquisite corpse\" - one writer begins the story and sends it to other writers. Each subsequent writer can only see the previously written section. Writermortis is currently a Ruby on Rails app but is going through an extensive re-write.",
+        description: "Writermortis is an online writing game that works like an \"exquisite corpse\" - one writer begins the story and sends it to other writers. Each subsequent writer can only see the previously written section. Writermortis is currently a Ruby on Rails app but is going through an extensive re-write in Ember.js.",
         url: "http://www.writermortis.com/",
-        code: "https://github.com/jasonblanchard/writermortis"
+        code: "https://github.com/jasonblanchard/writermortis",
+        image: 'writermortis.png'
+      },
+      {
+        id: 3,
+        name: "Booknodes",
+        description: "Booknodes is a Ruby on Rails app designed for taking notes while reading books. It helps you identify themes and underlying plot points by making it easy to tag and sort notes. The implementation makes heavy use of Rails UJS for a snappy, responsive user-interface.",
+        url: "http://booknodes.herokuapp.com/",
+        code: "https://github.com/jasonblanchard/booknodes",
+        image: 'booknodes.png'
       }
     ];
   }
